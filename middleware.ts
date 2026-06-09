f24bcc6cdae9aca5f9e6a46573a00909ecbd6b0a
yash <ysaxena4567@gmail.com>
2026-06-09 10:21:37 +0530

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/account(.*)",
+  "/dashboard(.*)",
+  "/history(.*)",
+  "/settings(.*)",
+  "/payment/success(.*)",
+  "/api/account(.*)",
+  "/api/feedback(.*)",
+  "/api/history(.*)",
+  "/api/payments(.*)",
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (isProtectedRoute(request)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
